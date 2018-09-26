c909c2624de4aba2087205f86bfd97d5cc108802
pexcn <i@pexcn.me>
2018-09-26 10:34:11 +0800

    relieve warning caused by the fucking mapper library

---
+++ src/main/java/me/pexcn/demo/EmptyMapper.java
@@ -0,0 +1,13 @@
+package me.pexcn.demo;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * The fucking mapper library has some problem: https://github.com/abel533/Mapper/issues/421
+ *
+ * @author pexcn
+ * @date 2018-09-26
+ */
+@Mapper
+public interface EmptyMapper {
+}
